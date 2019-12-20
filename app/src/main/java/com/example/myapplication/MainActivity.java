e6bdf8b3428dbbb67f05289d26c6b44b5bd0e6c2
lichao88 <lichao88@163.com>
2019-12-20 10:33:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.surpass.paykey.PayPassDialog;
+import com.surpass.paykey.PayPassView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        payDialog();
+    }
+    /**
+     * 余额支付弹框
+     */
+    private void payDialog() {
+        final PayPassDialog dialog=new PayPassDialog(this);
+        dialog.getPayViewPass()
+                .setShowPrice("100")
+                .setPayClickListener(new PayPassView.OnPayClickListener() {
+                    @Override
+                    public void onPassFinish(String passContent) {
+                        //6位输入完成回调
+                        Toast.makeText(MainActivity.this, ""+passContent, Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onPayClose() {
+                        dialog.dismiss();
+                        //关闭回调
+                    }
+                    @Override
+                    public void onPayForget() {
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, "忘记密码", Toast.LENGTH_SHORT).show();
+                        //点击忘记密码回调
+                    }
+                });
+    }
+}
